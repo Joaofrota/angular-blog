e78441f8e1dc71c0f98e23100bee54007a34b6d6
joao <jp.a.frota@gmail.com>
2023-10-19 15:47:01 -0300

    firts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './componets/menu-title/menu-title.component';
+import { MenuBarComponent } from './componets/menu-bar/menu-bar.component';
+import { SmallCardComponent } from './componets/small-card/small-card.component';
+import { BigCardComponent } from './componets/big-card/big-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+    SmallCardComponent,
+    BigCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
